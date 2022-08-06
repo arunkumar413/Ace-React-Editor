af237cf4a874ff47bd73163a7fbd27ea7b89e50d
arunkumar413 <arunkumar413@gmail.com>
2022-08-06 21:09:48 +0530

    save

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,6 +1,46 @@
 import React from "react";
 import { ReactComponent as SettingsIcon } from "../icons/settingsIcon.svg";
 
+[
+  "ambiance",
+  "chaos",
+  "chrome",
+  "clouds",
+  "clouds_midnight",
+  "cobalt",
+  "crimson_editor",
+  "dawn",
+  "dracula",
+  "dreamweaver",
+  "eclipse",
+  "github",
+  "gob",
+  "gruvbox",
+  "idle_fingers",
+  "iplastic",
+  "katzenmilch",
+  "kr_theme",
+  "kuroir",
+  "merbivore",
+  "merbivore_soft",
+  "mono_industrial",
+  "monokai",
+  "pastel_on_dark",
+  "solarized_dark",
+  "solarized_light",
+  "sqlserver",
+  "terminal",
+  "textmate",
+  "tomorrow",
+  "tomorrow_night",
+  "tomorrow_night_blue",
+  "tomorrow_night_bright",
+  "tomorrow_night_eighties",
+  "twilight",
+  "vibrant_ink",
+  "xcode",
+];
+
 export function SettingsPage(props) {
   return (
     <div className="Settings">
